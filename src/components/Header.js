14652964482bd9f1aa450952f926d5f17a111db3
Tina <radostinadimitrova@gmail.com>
2018-11-02 00:49:47 +0200

    feat: Creating a basic  Navbar

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Navbar, Nav, NavItem, FormGroup, FormControl, Button } from 'react-bootstrap';
+
+class Header extends React.Component {
+    render() {
+        return(
+            <div className="navbar">
+            <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                <a href="#brand">Logo</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">
+                    Home
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                    Contacts
+                </NavItem>
+                <NavItem eventKey={3} href="#">
+                    Latest Work
+                </NavItem>
+              </Nav>
+               <Navbar.Form pullRight>
+                <FormGroup>
+                <FormControl type="text" placeholder="Search" />
+                </FormGroup>{' '}
+                <Button type="submit">Submit</Button>
+               </Navbar.Form>
+            </Navbar.Collapse>
+            </Navbar>
+</div>
+   )
+ }
+
+}
+
+export default Header;
