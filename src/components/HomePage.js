b9de3791034bb1be7ff54f13a646a39813dffe7e
Tina <radostinadimitrova@gmail.com>
2018-11-04 23:49:34 +0200

    feat: Adding a new row and cols and insert social icons to the home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Jumbotron, Button, Grid, Row, Col } from 'react-bootstrap';
+import { SocialIcon } from 'react-social-icons';
 
 
 class HomePage extends React.Component {
     render() {
         return (
+          <Grid>
             <Jumbotron className="mainPage">
             <h1>Hi, I am Tina. I am Front-end Web Developer</h1>
             
@@ -12,6 +14,20 @@ class HomePage extends React.Component {
               <Button bsStyle="primary">Learn more, what i do</Button>
             </p>
           </Jumbotron>
+            
+          <Row className="show-grid">
+            <Col xs={4}>
+                <SocialIcon url="https://www.linkedin.com/in/radostina-dimitrova" />
+            </Col>
+            <Col xs={4}>
+                <SocialIcon className="icon" url="https://github.com/cristalnaya" />
+            </Col>
+            <Col xs={4}>
+                <SocialIcon url="https://codepen.io/dashboard/" />
+            </Col>
+           </Row>
+  
+         </Grid>
 
         )
     }
