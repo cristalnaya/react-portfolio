cd17b05cdf8edb55c57239740f9ffa83f889c45e
Tina <radostinadimitrova@gmail.com>
2018-11-11 22:34:05 +0200

    style: Organizing about page styles and structure

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import { Grid, Col } from 'react-bootstrap';
-import { FaJs, FaHtml5, FaCss3, FaReact } from 'react-icons/fa';
+import { Grid, Row, Col } from 'react-bootstrap'
+import '../scss/About.scss'
 
 
 class About extends React.Component {
     render() {
         return(
-            <div>
-              <Grid className="about-page">
-                           
-                <Col className="about-left" xs={12} sm={6} md={3} lg={6}>
-                    <h2 className="skills">Skills</h2>                    
-                <FaHtml5 className="skill-type" />
-                <FaCss3 className="skill-type" />
-                <FaJs className="skill-type" />
-                <FaReact className="skill-type" />
-
-                  
-                </Col>        
-               <Col className="about-right" xs={12} sm={6} lg={6}>
-                    <h2 className="info">About</h2>
-                    
-               </Col>
-             
-             </Grid>  
+            <div id="about">
+            <div className="hr-line"></div>
+             <Grid>            
+              <Row className="show-grid">
+                <Col className="skills-container" xs={12} md={10} lg={8}>
+                  <h2 className="skill-header">Skills</h2>
+                  <ul className="skills">
+                    <li className="skill-type">HTML5</li>
+                    <li className="skill-type">CSS3</li>
+                    <li className="skill-type">JavaScript</li>
+                    <li className="skill-type">React</li>
+                    <li className="skill-type">ES6</li>
+                    <li className="skill-type">jQuery</li>
+                    <li className="skill-type">AJAX</li>
+                    <li className="skill-type">Git</li>
+                    <li className="skill-type">Sass</li>
+                    <li className="skill-type">Jasmine</li>
+                  </ul> 
+                  </Col>
+                  <Col className="about-container" xs={8} md={4} lg={4}>
+                    <h2 className="about-header">What About Me</h2>
+                        <p>Front-end web developer, lifelong learner, self-taught, passionate about achieving excellence in my coding.</p>
+                  </Col>
+              </Row>
+                </Grid>
              </div> 
 
         )
