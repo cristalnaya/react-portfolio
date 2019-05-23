752fcb050ee9940402be9324019a5e61d8b497ef
Tina <radostinadimitrova@gmail.com>
2019-05-23 03:19:49 +0300

    feat: Create animated arrow icon

--- src/components/About.js
+++ src/components/About.js
@@ -1,37 +1,30 @@
-import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap'
-// import '../scss/About.scss'
+import React, { Component } from 'react';
+import { FaAngleDown } from 'react-icons/fa';
+import scrollToElement from 'scroll-to-element';
 
 
-class About extends React.Component {
-    render() {
-        return(
-            <div id="about">
-            <div className="hr-line"></div>
-             <Grid>            
-              <Row className="show-grid">
-                <Col className="skills-container" xs={12} md={10} lg={8}>
-                  <h2 className="skill-header">Skills</h2>
-                  <ul className="skills">
-                    <li className="skill-type">HTML5</li>
-                    <li className="skill-type">CSS3</li>
-                    <li className="skill-type">JavaScript</li>
-                    <li className="skill-type">React</li>
-                    <li className="skill-type">ES6</li>
-                    <li className="skill-type">jQuery</li>
-                    <li className="skill-type">AJAX</li>
-                    <li className="skill-type">Git</li>
-                    <li className="skill-type">Sass</li>
-                    <li className="skill-type">Jasmine</li>
-                  </ul> 
-                  </Col>
-                  <Col className="about-container" xs={9} md={4} lg={4}>
-                    <h2 className="about-header">What About Me</h2>
-                        <p>I am a Front-end web developer, lifelong learner, self-taught, passionate about achieving excellence in my coding.</p>
-                  </Col>
-              </Row>
-                </Grid>
-             </div> 
+class About extends Component {
+
+  handleArrowClick(el) {
+    scrollToElement(el);
+}
+
+  render() {
+    return(
+        <div id="about">
+          
+          <div className="about-container" style={{display: 'flex',
+          flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '70%', margin: 'auto', padding: '0 20px 20px',}}>
+                <h2 className="about-header">What About Me</h2>
+                  <p>Loves JavaScript and learning new stuff. Travel enthusiast, healthy food addicted. Currently a freelancer. <br/> (｡◕‿◕｡)</p>
+           </div>
+            <span className="arrow"
+                onClick={() =>
+                this.handleArrowClick('#latest-work')}>
+                <FaAngleDown 
+                className="arrow-down" />
+            </span>   
+          </div> 
 
         )
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
 import { SocialIcon } from 'react-social-icons';
 import { FaAngleDown } from 'react-icons/fa';
 import scrollToElement from 'scroll-to-element'
+import { slideInDown } from 'react-animations';
+import styled, { keyframes } from 'styled-components';
 
-class Home extends React.Component {
+const SlideInDown = styled.div`animation: 2s ${keyframes`${slideInDown}`} 2s`;
+
+class Home extends Component {
 
     handleArrowClick(el) {
         scrollToElement(el);
@@ -15,7 +19,10 @@ class Home extends React.Component {
         <div id="home">
         <Grid>
         <Jumbotron>
-            <h1>Hi, I am Tina. I am Front-end Web Developer</h1>
+        <SlideInDown>
+            <h1 style={{fontFamily: "'Roboto Mono', monospace"}}>Hi, I am Tina!</h1>
+        </SlideInDown>
+            <p>Front-end Web Developer</p>
               <span className="arrow"
                     onClick={() =>
                     this.handleArrowClick('#about')}>
@@ -26,15 +33,22 @@ class Home extends React.Component {
             
           <Row className="show-grid social-icons">
             <Col xs={4}>
+            <SlideInDown>
                 <SocialIcon url="https://www.linkedin.com/in/radostina-dimitrova" />
+            </SlideInDown>
             </Col>
             <Col xs={4}>
+            <SlideInDown>
                 <SocialIcon url="https://github.com/cristalnaya" />
+            </SlideInDown>
             </Col>
-            <Col xs={4}>
-                <SocialIcon
-                url="https://codepen.io/dashboard/" />
-            </Col>
+                <Col xs={4}>
+                    <SlideInDown>
+                    <SocialIcon
+                    url="https://codepen.io/dashboard/" />
+                    </SlideInDown>
+                </Col>
+
            </Row>
           </Grid>
           </div>
