b9918b6bafe5e77eb4867e33b77ca7261a6d2e23
Tina <radostinadimitrova@gmail.com>
2019-05-23 03:16:32 +0300

    feat: Add sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,84 @@
 import React, { Component } from 'react';
 import './App.scss';
 import './scss/About.scss'
-import Header from './components/Header';
 import Home from './components/Home'
 import About from './components/About'
 import LatestWork from './components/LatestWork'
+import Sidebar from "./components/Sidebar";
+import Footer from './components/Footer';
+import OverwatchSideNav from 'overwatch-side-nav/lib';
+import Skills from './components/Skills';
+import { images } from './components/images';
+import { icons } from './components/icons';
+
 
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+         offset: 0
+    }
+}
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.parallaxShift);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.parallaxShift);
+  }
+  parallaxShift = () => {
+    this.setState({
+      offset: window.pageYOffset
+    });
+  };
+
   render() {
+
+    const items = [
+  
+      {
+        label: 'Latest Work',
+        id: 'latest-work',
+        icon:  '<img src="https://i.imgur.com/U4OhEvQ.png" alt="Toolkit" style="margin-top: 2px;" />',
+      },
+    
+      {
+          label: 'About me',
+          id: 'about',
+          icon:  '<img src="https://i.imgur.com/SFAKn3i.png" alt="User icon" style="margin-top: 2px;" />',
+      },
+      {
+        label: 'Skills',
+        id: 'skills',
+        icon:  '<img src="https://i.imgur.com/gsp0Vs2.png" alt="Code icon" />',
+    },
+      {
+          label: 'Home',
+          id: 'home',
+          icon:  '<img src="https://i.imgur.com/JN2i07H.png" alt="home" style="margin-top: 2px;" />',
+      }
+    ]
+    
+    const Sections = () => <div>{[...Array(items.length)].map((_, index) =>
+        <section id={items[index].id} key={index}>
+            {`${index + 1}`}
+        </section>)
+    }
+
+    </div>
     return (
       <div className="App">
-      <Header />
-      <Home />
-      <About />
-      <LatestWork />
+        <Sidebar
+          distance={20}>
+            <OverwatchSideNav items={items} />
+        </Sidebar>
+          <Home />
+          <About />
+          <Skills icons={icons} />
+          <LatestWork images={images}/>
+          <Footer />
   </div>
     )
   }
