c0b11b144643e21b5c6b21cfa5a9efc703deb11b
Tina <radostinadimitrova@gmail.com>
2019-05-23 03:17:14 +0300

    feat: Create data files for the images and icons

---
+++ src/components/images.js
@@ -0,0 +1,39 @@
+export const images =
+[
+    {
+        id: 1,
+        src: '/img/arcade-game.png',
+        alt: 'arcade game',
+        title: 'arcade game',
+        projectUrl: 'https://github.com/cristalnaya/frontend-nanodegree-arcade-game'
+    },
+    {
+        id: 2,
+        src: '/img/memory-game.png',
+        alt: 'memory game',
+        title: 'memory game',
+        projectUrl: 'https://github.com/cristalnaya/project-memory-game'
+    },
+    {
+        id: 5,
+        src: '/img/restaurant-reviews.png',
+        alt: 'Restaurant Reviews',
+        title: 'Restaurant Reviews',
+        projectUrl: 'https://github.com/cristalnaya/fend-restaurant-stage-1'
+    },
+    {
+        id: 3,
+        src: '/img/myreads.png',
+        alt: 'MyReads',
+        title: 'MyReads',
+        projectUrl: 'https://github.com/cristalnaya/reactnd-project-myreads'
+    },
+    {
+        id: 4,
+        src: '/img/neighborhood-map.png',
+        alt: 'Neighborhood Map',
+        title: 'Neighborhood Map',
+        projectUrl: 'https://github.com/cristalnaya/react-neighbourhood-map'
+    },
+]
+
