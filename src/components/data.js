322bf6ba11bba11f2282d7d4a96c1fadd37b42a3
Tina <radostinadimitrova@gmail.com>
2019-05-25 03:35:49 +0300

    refactor: Update rendered data file

---
+++ src/components/data.js
@@ -0,0 +1,102 @@
+export const images =
+[
+    {
+        id: 1,
+        src: '/img/arcade-game.png',
+        alt: 'arcade game',
+        title: 'arcade game',
+        projectUrl: 'https://github.com/cristalnaya/frontend-nanodegree-arcade-game'
+    },
+    {
+        id: 2,
+        src: '/img/memory-game.png',
+        alt: 'memory game',
+        title: 'memory game',
+        projectUrl: 'https://github.com/cristalnaya/project-memory-game'
+    },
+    {
+        id: 5,
+        src: '/img/restaurant-reviews.png',
+        alt: 'Restaurant Reviews',
+        title: 'Restaurant Reviews',
+        projectUrl: 'https://github.com/cristalnaya/fend-restaurant-stage-1'
+    },
+    {
+        id: 3,
+        src: '/img/myreads.png',
+        alt: 'MyReads',
+        title: 'MyReads',
+        projectUrl: 'https://github.com/cristalnaya/reactnd-project-myreads'
+    },
+    {
+        id: 4,
+        src: '/img/neighborhood-map.png',
+        alt: 'Neighborhood Map',
+        title: 'Neighborhood Map',
+        projectUrl: 'https://github.com/cristalnaya/react-neighbourhood-map'
+    },
+]
+
+export const icons = 
+[
+    {
+        id: 1,
+        imgSrc: '/img/html5.png',
+        alt: 'html5',
+        title: 'HTML5'
+    },
+    {
+        id: 2,
+        imgSrc: '/img/css3.png',
+        alt: 'css3',
+        title: 'CSS3'
+    },
+    {
+        id: 3,
+        imgSrc: '/img/js.png',
+        alt: 'JavaScript',
+        title: 'JavaScript'
+    },
+    {
+        id: 4,
+        imgSrc: '/img/react.png',
+        alt: 'React',
+        title: 'React'
+    },
+    {
+        id: 5,
+        imgSrc: '/img/git.png',
+        alt: 'GIT',
+        title: 'GIT'
+    },
+    {
+        id: 6,
+        imgSrc: '/img/ajax.png',
+        alt: 'AJAX',
+        title: 'AJAX'
+    },
+    {
+        id: 7,
+        imgSrc: '/img/jest.png',
+        alt: 'JEST',
+        title: 'JEST'
+    },
+    {
+        id: 8,
+        imgSrc: '/img/jasmine.png',
+        alt: 'Jasmine',
+        title: 'Jasmine'
+    },
+    {
+        id: 9,
+        imgSrc: '/img/sass.png',
+        alt: 'Sass',
+        title: 'Sass'
+    },
+    {
+        id: 10,
+        imgSrc: '/img/jquery.png',
+        alt: 'jQuery',
+        title: 'jQuery'
+    },
+]
