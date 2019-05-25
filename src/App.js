f81ba839780b928108279f4ab272c717514437d8
Tina <radostinadimitrova@gmail.com>
2019-05-25 03:41:21 +0300

    feat: Create home page changing  colors bg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.scss';
-import './scss/About.scss'
 import Home from './components/Home'
 import About from './components/About'
 import LatestWork from './components/LatestWork'
@@ -8,32 +7,41 @@ import Sidebar from "./components/Sidebar";
 import Footer from './components/Footer';
 import OverwatchSideNav from 'overwatch-side-nav/lib';
 import Skills from './components/Skills';
-import { images } from './components/images';
-import { icons } from './components/icons';
+import { images, icons } from './components/data';
+// import { icons } from './components/icons';
 
-
+const colors = {
+  default: '#f3c1c6',
+  green: '#05a19c',
+  pink: '#d698b9',
+  mustard: '#fdd043',
+  orange: '#ffba90',
+  darkgreen: '#004a2f',
+  grass: '#a7d129',
+  purple: '#6f0765'
+}
 
 class App extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
-         offset: 0
-    }
+        colors: colors.default,
+    };
+
+    this.interval = setInterval(() => {
+      const randomColor = colors[Object.keys(colors)[Math.floor(Math.random() * Object.keys(colors).length)
+      ]
+    ];
+    this.setState(() => ({colors: randomColor}))
+    }, 3000);
 }
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.parallaxShift);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.parallaxShift);
-  }
-  parallaxShift = () => {
-    this.setState({
-      offset: window.pageYOffset
-    });
-  };
 
+componentWillUnmount() {
+  clearInterval(this.interval);
+}
+
+ 
   render() {
 
     const items = [
@@ -43,21 +51,20 @@ class App extends Component {
         id: 'latest-work',
         icon:  '<img src="https://i.imgur.com/U4OhEvQ.png" alt="Toolkit" style="margin-top: 2px;" />',
       },
-    
       {
-          label: 'About me',
-          id: 'about',
-          icon:  '<img src="https://i.imgur.com/SFAKn3i.png" alt="User icon" style="margin-top: 2px;" />',
+        label: 'About me',
+        id: 'about',
+        icon:  '<img src="https://i.imgur.com/SFAKn3i.png" alt="User icon" style="margin-top: 2px;" />',
       },
       {
         label: 'Skills',
         id: 'skills',
         icon:  '<img src="https://i.imgur.com/gsp0Vs2.png" alt="Code icon" />',
-    },
+      },
       {
-          label: 'Home',
-          id: 'home',
-          icon:  '<img src="https://i.imgur.com/JN2i07H.png" alt="home" style="margin-top: 2px;" />',
+        label: 'Home',
+        id: 'home',
+        icon:  '<img src="https://i.imgur.com/JN2i07H.png" alt="home" style="margin-top: 2px;" />',
       }
     ]
     
@@ -69,17 +76,21 @@ class App extends Component {
 
     </div>
     return (
-      <div className="App">
-        <Sidebar
-          distance={20}>
-            <OverwatchSideNav items={items} />
-        </Sidebar>
-          <Home />
-          <About />
-          <Skills icons={icons} />
-          <LatestWork images={images}/>
-          <Footer />
-  </div>
+      <div className="App"
+      style={{ backgroundColor: this.state.colors,
+      height: '100vh' }}>
+       
+          <Sidebar
+            distance={20}>
+              <OverwatchSideNav items={items} />
+          </Sidebar>
+          
+            <Home />
+            <About />
+            <Skills icons={icons} />
+            <LatestWork images={images}/>
+            <Footer />
+    </div>
     )
   }
 }
