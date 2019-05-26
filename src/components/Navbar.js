c9db1ae5852a17a6128cf80519e2c21f4e11f857
Tina <radostinadimitrova@gmail.com>
2019-05-27 02:44:18 +0300

    feat: Create fixed top Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBIcon } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Scrollchor from 'react-scrollchor';
+
+
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+       <Router>
+        <MDBNavbar className="font-weight-bolder h3-responsive" color="elegant-color" fixed="top" dark expand="md" scrolling transparent>
+            <MDBNavbarBrand className="flex-center hoverable">
+            <a href="https://github.com/cristalnaya/react-portfolio" className="text-white h4-responsive waves-effect waves-light" target="_blank" rel="noopener noreferrer"><MDBIcon fab icon="github-alt" size="lg" />GitHub Repo</a>
+            </MDBNavbarBrand>
+            {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+            <MDBCollapse isOpen={this.state.collapse} navbar>
+            <MDBNavbarNav right>
+                <MDBNavItem className="p-2 hoverable" active>
+                <Scrollchor to="#home" className="nav-link">
+                Home
+                </Scrollchor>
+                </MDBNavItem>
+                <MDBNavItem className="p-2 hoverable">
+                <Scrollchor to="#about" className="nav-link">
+                About
+                </Scrollchor>
+                </MDBNavItem>
+                <MDBNavItem className="p-2 hoverable">
+                <Scrollchor to="#skills" className="nav-link">
+                Skills
+                </Scrollchor>
+                </MDBNavItem>
+                <MDBNavItem className="p-2 hoverable">
+                <Scrollchor to="#latest-work" className="nav-link">
+                Latest Work
+                </Scrollchor>
+                </MDBNavItem>
+            </MDBNavbarNav>
+            </MDBCollapse>
+        </MDBNavbar>
+       </Router>
+    </div>
+    );
+  }
+}
+
+export default Navbar;
